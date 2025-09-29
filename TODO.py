08176b15025ebe5a8d50b5f21ba02d6a7125f891
misti <idlkz2019@gmail.com>
2025-09-29 17:55:40 +0500

    Ended ProductOrder + Full Payment

---
+++ TODO.py
@@ -0,0 +1,5 @@
+#TODO 1. Закончить логику поступления и движения товаров и отмены ProductOrderItem (HISTORY, VERIFICATION_CODE)
+#TODO 2. Реализовать бронирование полей с покупкой
+#TODO 3. Реализовать запись в секцию путем заполнения анкеты
+#TODO 4. Добавить Firebase Notifications + Notifications for User
+#TODO 5. Реализовать Schedulers (1 - Ticketon, 2 - Product)
