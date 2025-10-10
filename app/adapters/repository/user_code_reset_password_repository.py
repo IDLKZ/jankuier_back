f9f953777e33453f19657280b320db266101a473
misti <idlkz2019@gmail.com>
2025-10-10 12:11:18 +0500

    Any Hall Anymore

--- app/adapters/repository/user_code_reset_password_repository.py
+++ app/adapters/repository/user_code_reset_password_repository.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
 from app.adapters.repository.base_repository import BaseRepository
 from app.entities.user_reset_password_code_entity import UserCodeResetPasswordEntity
@@ -7,3 +10,8 @@ from app.entities.user_reset_password_code_entity import UserCodeResetPasswordEn
 class UserCodeResetPasswordRepository(BaseRepository[UserCodeResetPasswordEntity]):
     def __init__(self, db: AsyncSession):
         super().__init__(UserCodeResetPasswordEntity, db)
+
+    def default_relationships(self) -> list[Any]:
+        return [
+            selectinload(self.model.user),
+        ]
